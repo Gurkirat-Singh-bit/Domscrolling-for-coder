a6f8a4b48bc908f7655110e155b046ee61044734
GURKIRAT SINGH <mr.gurkirat.singh.no1@gmail.com>
2025-12-22 02:51:49 +0530

    Fix: scattered Components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,7 +9,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/ui/theme-provider"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
